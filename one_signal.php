6d2351e2db741773790a56c8be2ad2c708e74de9
Armen96 <barsegyan96armen@gmail.com>
2019-03-03 23:04:37 +0400

    one signal notify js 2

--- one_signal.php
+++ one_signal.php
@@ -53,7 +53,7 @@ print("\n");
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-
+        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
 </head>
 <body>
 
